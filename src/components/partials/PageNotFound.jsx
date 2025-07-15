a20c966dc023d9b76b533203de51ae3e99d62dbb
aaairam <magildaceria@gmail.com>
2025-07-15 10:39:26 +0800

    commit

--- src/components/partials/PageNotFound.jsx
+++ src/components/partials/PageNotFound.jsx
@@ -3,13 +3,9 @@ import React from "react";
 const PageNotFound = () => {
   return (
     <>
-      <div className="p-44 bg-blue-400 text-white">
-        <h1 className="font-bold text-[20rem] text-center">
-          404
-        </h1>
-        <p className="text-9xl font-bold text-center">
-          Oops...Page Not Found
-        </p>
+      <div className="p-44 bg-white text-center">
+        <h1 className="font-bold text-[20rem] text-center">404</h1>
+        <p className="text-9xl font-bold text-center">Oops...Page Not Found</p>
       </div>
     </>
   );
